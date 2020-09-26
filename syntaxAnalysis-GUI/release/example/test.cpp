a987f5ba0561cc7af5aea1d4b248f35face6f23d
crazy_fz <2454519050@qq.com>
2020-09-26 12:53:17 +0800

    update release

--- syntaxAnalysis-GUI/release/example/test.cpp
+++ syntaxAnalysis-GUI/release/example/test.cpp
@@ -1,13 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    /* string 字符串
+int main()
+{
+
+    char c = '\n'; // 带转义char
+    char ch = 'c'; // 一般char
+    double f=-5.3e-5;
+    int a=0xAef; //十六进制
+    /* 
+    带转义string
     */
-    string str="abc de";
-    char c='\n'; // char 字符
-    int len=str.length();
-    for(int i=0;i<len;++i)
-        cout<<str[i];
+    string str = "abc\n \\ 中文"; // 空格，测试压缩
+    a=1
+// 测试压缩功能，这样写是可以编译的
+    +1;
+    
+    int len = str.length();
+    for (int i = 0; i < len; ++i)
+        cout << str[i];
 
     return 0;
 }
